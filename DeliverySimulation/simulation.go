7fe6329fa4685975b2d40c58b29a05ce35675f4b
Eric Haggar <ehagg014@uottawa.ca>
2019-02-03 23:17:33 -0500

    Created initializer methods, interface and changed struct pointers

--- DeliverySimulation/simulation.go
+++ DeliverySimulation/simulation.go
@@ -1,5 +1,11 @@
 package main
 
+//Transporter is an interface which defines addLoad and print method signatures
+type Transporter interface {
+	addLoad(trip Trip) error
+	print()
+}
+
 //Trip defines a trip from Ottawa to Montreal or Toronto
 type Trip struct {
 	destination string
@@ -19,19 +25,57 @@ type Transportation struct {
 
 //Truck is a type of vehicle to be used for the delivery
 type Truck struct {
-	*Transportation
+	Transportation
 }
 
 //Pickup is a type of vehicle to be used for the delivery
 type Pickup struct {
-	*Transportation
+	Transportation
 	isPrivate bool
 }
 
 //TrainCar is a type of vehicle to be used for the delivery
 type TrainCar struct {
-	*Transportation
+	Transportation
 	railway string
+}
+
+//NewTruck returns an initialized Truck
+func NewTruck() *Truck {
+	return &Truck{Transportation{
+		vehicle:     "",
+		name:        "",
+		destination: "",
+		speed:       40,
+		capacity:    10,
+		load:        0,
+	}}
+}
+
+//NewPickUp returns an initialized Pickup
+func NewPickUp() *Pickup {
+	return &Pickup{Transportation{
+		vehicle:     "",
+		name:        "",
+		destination: "",
+		speed:       60,
+		capacity:    2,
+		load:        0,
+	},
+		true}
+}
+
+//NewTrainCar returns an initialized TrainCar
+func NewTrainCar() *TrainCar {
+	return &TrainCar{Transportation{
+		vehicle:     "",
+		name:        "",
+		destination: "",
+		speed:       30,
+		capacity:    30,
+		load:        0,
+	},
+		""}
 }
 
 func main() {
